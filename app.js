b6bcbf70f23354604cac083b41b44e8e03b9bef3
Jeremy Bchiri <jerem.b95@gmail.com>
2014-05-04 17:07:09 +0200

    Demarage

---
+++ app.js
@@ -0,0 +1,8 @@
+var id = 'Jerem';
+var mp = 'Daniel';
+
+var id_test=prompt("Entrez votre identifiant");
+var mp_test=prompt("Entrez votre mot de passe:");
+var test;
+test= id == id_test , mp == mp_test;
+alert(test);
---
+++ test.js
@@ -0,0 +1,3 @@
+var a = 'Jerem', b = 'Jeremy' , c = 5, result;
+result = a == b;
+alert(result);
